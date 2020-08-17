17b43b0af92c284c0123d7a7b7af4b6d5bc9de93
琴卓 <1043665802@qq.com>
2020-08-17 18:52:17 +0800

    新增序列化相关代码

---
+++ Runtime/QSerialize.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Collections;
+
+namespace QTool
+{
+    public class QSValueAttribute : Attribute
+    {
+        
+    }
+    public class QSIgnoreAttribute : Attribute
+    {
+
+    }
+    public class QMemeberInfo
+    {
+        public Type type;
+        public Action<object, object> set;
+        public Func<object, object> get;
+    }
+    public class QTypeInfo
+    {
+        static bool IsQSValue(MemberInfo info, bool isPublic = true)
+        {
+            if (!isPublic && info.GetCustomAttribute<QSValueAttribute>() == null)
+            {
+                return false;
+            }
+            if (info.GetCustomAttribute<QSIgnoreAttribute>() != null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public List<QMemeberInfo> memberList = new List<QMemeberInfo>();
+        public bool IsList;
+        private QTypeInfo(Type type)
+        {
+
+            IsList = (type.GetInterface(typeof(IList<>).FullName, true) != null);
+            if (IsList) return;
+            FieldInfo[] fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (var item in fields)
+            {
+                if (IsQSValue(item, item.IsPublic))
+                {
+                    var qMember = new QMemeberInfo
+                    {
+                        type = item.FieldType,
+                        set = item.SetValue,
+                        get = item.GetValue,
+                    };
+                    memberList.Add(qMember);
+                }
+
+            }
+            var infos = type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (var item in infos)
+            {
+
+                if (IsQSValue(item, item.Name != "Item" && item.CanRead && item.CanWrite && item.SetMethod.IsPublic && item.GetMethod.IsPublic))
+                {
+                    var qMember = new QMemeberInfo
+                    {
+                        type=item.DeclaringType,
+                        set = item.SetValue,
+                        get = item.GetValue,
+                    };
+                    memberList.Add(qMember);
+                }
+            }
+        }
+      
+        public static QTypeInfo Get(Type type)
+        {
+            return new QTypeInfo(type);
+        }
+       
+    }
+    public static class QSerialize
+    {
+        public static Byte[] Serialize<T>(T value)
+        {
+            using (MemoryStream memory=new MemoryStream())
+            {
+                using (BinaryWriter writer=new BinaryWriter(memory))
+                {
+                  
+                    var type = typeof(T);
+                    writer.WriteValue(type,value);
+                    return memory.ToArray();
+                }
+            }
+        }
+        public static T Deserialize<T>(byte[] bytes)
+        {
+            using (MemoryStream memory = new MemoryStream(bytes))
+            {
+                using (BinaryReader reader = new BinaryReader(memory))
+                {
+                    var type = typeof(T);
+                    return (T)reader.ReadValue(type);
+                }
+            }
+        }
+        static void ForeachArray(Array array,int deep,int[] indexArray,Action<object> Call)
+        {
+            for (int i = 0; i < array.GetLength(deep); i++)
+            {
+                indexArray[deep] = i;
+                if (deep+1 < indexArray.Length)
+                {
+                    ForeachArray(array, deep + 1, indexArray,Call);
+                }
+                else
+                {
+                    Call?.Invoke(array.GetValue(indexArray));
+                }
+              
+            }
+        }
+   
+        static object CreateInstance(Type type,params object[] args)
+        {
+            return Activator.CreateInstance(type, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, args, null);
+        }
+        static object ReadValue(this BinaryReader reader, Type type)
+        {
+            var typeCode = Type.GetTypeCode(type);
+            var valueTypeCode= (TypeCode)reader.ReadByte();
+            switch (typeCode)
+            {
+                case TypeCode.Boolean:
+                    return reader.ReadBoolean();
+                case TypeCode.Byte:
+                    return reader.ReadByte();
+                case TypeCode.Char:
+                    return reader.ReadChar();
+                case TypeCode.DateTime:
+                    return new DateTime(reader.ReadInt64());
+                case TypeCode.DBNull:
+                    return null;
+                case TypeCode.Decimal:
+                    return (decimal)reader.ReadDouble();
+                case TypeCode.Double:
+                    return reader.ReadDouble();
+                case TypeCode.Empty:
+                    return null;
+                case TypeCode.Int16:
+                    return reader.ReadInt16();
+                case TypeCode.Int32:
+                    return reader.ReadInt32();
+                case TypeCode.Int64:
+                    return reader.ReadInt64();
+                case TypeCode.Object:
+                    if (type.IsArray)
+                    {
+                        var elementType = type.GetElementType();
+                        var rank = reader.ReadInt32();
+                        var indexArray = new int[rank];
+                        var count = 1;
+                        for (int i = 0; i < rank; i++)
+                        {
+                            indexArray[i] = reader.ReadInt32();
+                            count *= indexArray[i];
+                        }
+                        var array =(Array)CreateInstance(type, count) ;
+                        ForeachArray(array, 0, indexArray, (obj)=> { array.SetValue( reader.ReadValue(elementType), indexArray ); });
+                        return array;
+                    }
+                    else 
+                    {
+                        var obj =  CreateInstance(type);
+                        QTypeInfo typeInfo = QTypeInfo.Get(type);
+                        foreach (var item in typeInfo.memberList)
+                        {
+                            item.set?.Invoke(obj, reader.ReadValue(item.type));
+                        }
+                        return obj;
+                    }
+                    return null;
+                case TypeCode.SByte:
+                    return reader.ReadSByte();
+                case TypeCode.Single:
+                    return reader.ReadSingle();
+                case TypeCode.String:
+                    return reader.ReadString();
+                case TypeCode.UInt16:
+                    return reader.ReadUInt16();
+                case TypeCode.UInt32:
+                    return reader.ReadUInt32();
+                case TypeCode.UInt64:
+                    return reader.ReadUInt64();
+                default:
+                    return null;
+            }
+        }
+        static void WriteValue(this BinaryWriter writer ,Type type ,object value)
+        {
+            var typeCode = Type.GetTypeCode(type);
+            writer.Write((byte)typeCode);
+            switch (typeCode)
+            {
+                case TypeCode.Boolean:
+                    writer.Write((bool)value);
+                    break;
+                case TypeCode.Byte:
+                    writer.Write((byte)value);
+                    break;
+                case TypeCode.Char:
+                    writer.Write((char)value);
+                    break;
+                case TypeCode.DateTime:
+                    writer.Write(((DateTime)value).Ticks);
+                    break;
+                case TypeCode.DBNull:
+                    break;
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                    writer.Write((double)value);
+                    break;
+                case TypeCode.Empty:
+                    break;
+                case TypeCode.Int16:
+                    writer.Write((Int16)value);
+                    break;
+                case TypeCode.Int32:
+                    writer.Write((Int32)value);
+                    break;
+                case TypeCode.Int64:
+                    writer.Write((Int64)value);
+                    break;
+                case TypeCode.Object:
+                    if (type.IsArray)
+                    {
+                        var array = value as Array;
+                        var elementType = type.GetElementType();
+                        writer.Write(array.Rank);
+                        for (int i = 0; i < array.Rank; i++)
+                        {
+                            writer.Write(array.GetLength(i));
+                        }
+                        var indexArray = new int[array.Rank];
+                        ForeachArray(array, 0, indexArray, (obj) => {writer.WriteValue(elementType,obj); });
+     
+                    }
+                    else
+                    {
+                        QTypeInfo typeInfo = QTypeInfo.Get(type);
+                        if (typeInfo.IsList)
+                        {
+                            var elementType = type.GenericTypeArguments[0];
+                            var list = value as IList;
+                            writer.Write(list.Count);
+                            foreach (var item in list)
+                            {
+                                writer.WriteValue(elementType, item);
+                         //       UnityEngine.Debug.LogError("value:"+ elementType+":" + item);
+                            }
+                        }
+                        else
+                        {
+                            foreach (var item in typeInfo.memberList)
+                            {
+                                writer.WriteValue(item.type, item.get(value));
+                            }
+                        }
+                       
+                    }
+                    break;
+                case TypeCode.SByte:
+                    writer.Write((SByte)value);
+                    break;
+                case TypeCode.Single:
+                    writer.Write((Single)value);
+                    break;
+                case TypeCode.String:
+                    writer.Write((string)value);
+                    break;
+                case TypeCode.UInt16:
+                    writer.Write((UInt16)value);
+                    break;
+                case TypeCode.UInt32:
+                    writer.Write((UInt32)value);
+                    break;
+                case TypeCode.UInt64:
+                    writer.Write((UInt64)value);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Test/Test.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QTool;
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public class TestClass
+    {
+        public int p1 {  get;  set; }
+        public float p2;
+        public int this[int x]
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+
+            }
+        }
+        public int this[int x,int y]
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+
+            }
+        }
+    }
+    public Vector3 v3 = new Vector3();
+    public List<int> list = new List<int> { 123,45,56 };
+    public List<int> blist = new List<int> { 123, 45, 56 };
+    public TestClass a = new TestClass { };
+    public int[] b;
+    public byte[] info;
+    [ContextMenu("test")]
+    public void TestFunc()
+    {
+        info = QSerialize.Serialize(list);
+     //   Debug.LogError(QSerialize.Deserialize<Vector3>(info));
+    
+
+    }
+}
