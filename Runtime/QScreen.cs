13181a7d8a070395b5d393c2c1e19a0d550487fd
琴卓 <1043665802@qq.com>
2021-02-01 17:22:20 +0800

    0.0.27 增加对屏幕分辨率的控制

---
+++ Runtime/QScreen.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+namespace QTool
+{
+    public static class QScreen
+    {
+
+        public static void SetResolution(int width, int height, bool fullScreen)
+        {
+#if UNITY_EDITOR
+            SetSize(width, height);
+#else
+         Screen.SetResolution(width, height, fullScreen);
+#endif
+        }
+
+#if UNITY_EDITOR
+
+        static object gameViewSizesInstance;
+        static MethodInfo getGroup;
+
+        static QScreen()
+        {
+            // gameViewSizesInstance  = ScriptableSingleton<GameViewSizes>.instance;
+            var sizesType = typeof(Editor).Assembly.GetType("UnityEditor.GameViewSizes");
+            var singleType = typeof(ScriptableSingleton<>).MakeGenericType(sizesType);
+            var instanceProp = singleType.GetProperty("instance");
+            getGroup = sizesType.GetMethod("GetGroup");
+            gameViewSizesInstance = instanceProp.GetValue(null, null);
+        }
+
+        private enum GameViewSizeType
+        {
+            AspectRatio, FixedResolution
+        }
+
+        private static void SetSize(int index)
+        {
+            var gvWndType = typeof(Editor).Assembly.GetType("UnityEditor.GameView");
+            var selectedSizeIndexProp = gvWndType.GetProperty("selectedSizeIndex",
+                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            var gvWnd = EditorWindow.GetWindow(gvWndType);
+            selectedSizeIndexProp.SetValue(gvWnd, index, null);
+        }
+
+
+
+        private static void AddCustomSize(GameViewSizeType viewSizeType, GameViewSizeGroupType sizeGroupType, int width, int height, string text)
+        {
+
+            var group = GetGroup(sizeGroupType);
+            var addCustomSize = getGroup.ReturnType.GetMethod("AddCustomSize"); // or group.GetType().
+            var gvsType = typeof(Editor).Assembly.GetType("UnityEditor.GameViewSize");
+            var ctor = gvsType.GetConstructor(new Type[] { typeof(int), typeof(int), typeof(int), typeof(string) });
+            var newSize = ctor.Invoke(new object[] { (int)viewSizeType, width, height, text });
+            addCustomSize.Invoke(group, new object[] { newSize });
+        }
+
+
+        private static int FindSize(GameViewSizeGroupType sizeGroupType, int width, int height)
+        {
+            var group = GetGroup(sizeGroupType);
+            var groupType = group.GetType();
+            var getBuiltinCount = groupType.GetMethod("GetBuiltinCount");
+            var getCustomCount = groupType.GetMethod("GetCustomCount");
+            int sizesCount = (int)getBuiltinCount.Invoke(group, null) + (int)getCustomCount.Invoke(group, null);
+            var getGameViewSize = groupType.GetMethod("GetGameViewSize");
+            var gvsType = getGameViewSize.ReturnType;
+            var widthProp = gvsType.GetProperty("width");
+            var heightProp = gvsType.GetProperty("height");
+            var indexValue = new object[1];
+            for (int i = 0; i < sizesCount; i++)
+            {
+                indexValue[0] = i;
+                var size = getGameViewSize.Invoke(group, indexValue);
+                int sizeWidth = (int)widthProp.GetValue(size, null);
+                int sizeHeight = (int)heightProp.GetValue(size, null);
+                if (sizeWidth == width && sizeHeight == height)
+                    return i;
+            }
+            return -1;
+        }
+
+        static object GetGroup(GameViewSizeGroupType type)
+        {
+            return getGroup.Invoke(gameViewSizesInstance, new object[] { (int)type });
+        }
+
+
+        private static GameViewSizeGroupType GetCurrentGroupType()
+        {
+            var getCurrentGroupTypeProp = gameViewSizesInstance.GetType().GetProperty("currentGroupType");
+            return (GameViewSizeGroupType)(int)getCurrentGroupTypeProp.GetValue(gameViewSizesInstance, null);
+        }
+
+
+        private static void SetSize(int width, int height)
+        {
+            int index = FindSize(GetCurrentGroupType(), width, height);
+            if (index == -1)
+            {
+                AddCustomSize(GameViewSizeType.FixedResolution, GetCurrentGroupType(), width, height, "");
+                index = FindSize(GetCurrentGroupType(), width, height);
+            }
+            if (index != -1)
+            {
+                SetSize(index);
+            }
+            else
+            {
+                Debug.LogError("设置游戏视窗分辨率出错 " + width.ToString() + "*" + height.ToString());
+            }
+        }
+#endif
+    }
+}
