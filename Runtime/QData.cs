dd3971bca417f91adbb11e8222e1475ad6573321
琴卓 <1043665802@qq.com>
2021-02-18 13:03:29 +0800

    增加Async判断

---
+++ Runtime/QData.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QData
+{
+#if Addressables
+    public static void Test()
+    {
+        Debug.LogError("test");
+    }
+#endif
+}
