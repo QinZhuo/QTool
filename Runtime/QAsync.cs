2db79eb429b6abc9c307f93ee1e44c66f634860c
琴卓 <1043665802@qq.com>
2021-02-18 15:14:26 +0800

    0.1.0 增加Synclist相关逻辑

--- Runtime/QAsync.cs
+++ Runtime/QAsync.cs
@@ -6,6 +6,7 @@ using System;
 using UnityEngine.AddressableAssets;
 namespace QTool.Async
 {
+  
     public abstract class AsyncList<ObjT>where ObjT:UnityEngine.Object
     {
         public static Dictionary<string, ObjT> objDic = new Dictionary<string, ObjT>();
@@ -44,7 +45,7 @@ namespace QTool.Async
                 return null;
             }
         }
-        public static void AsyncGet(string key,Action<ObjT> loadOver)
+        public static void GetAsync(string key,Action<ObjT> loadOver)
         {
             var obj = Get(key);
             if (obj!=null)
@@ -53,8 +54,134 @@ namespace QTool.Async
             }
             else
             {
-             //   var load=ad
+                var load = Addressables.LoadAssetAsync<ObjT>(key);
+                load.Completed += (result) =>
+                {
+                    if (result.Status == UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
+                    {
+                        Set(key, result.Result);
+                        loadOver?.Invoke(result.Result);
+                    }
+                    else
+                    {
+                        loadOver?.Invoke(null);
+                    }
+                };
             }
+        }
+        public static void Set(string key,ObjT obj)
+        {
+            if (!ContainsKey(key))
+            {
+                objDic.Add(key, obj);
+            }
+            else
+            {
+                objDic[key] = obj;
+            }
+        }
+        public static IEnumerator LoadLabelAsync()
+        {
+            if (LabelLoadOver) yield break;
+            Addressables.LoadAssetsAsync<ObjT>(Label, (result) =>
+            {
+                Set(result.name, result);
+            }).Completed+=(results)=> {
+                Debug.LogError("[" + Label + "]加载完成总数" + objDic.Count);
+                OnLabelLoadOver?.Invoke();
+                OnLabelLoadOver = null;
+                LabelLoadOver = true;
+            };
+            while (!LabelLoadOver)
+            {
+                yield return null;
+            }
+        }
+   
+    }
+    public abstract class PrefabAsyncList: AsyncList<GameObject> 
+    {
+        static Dictionary<string, ObjectPool<GameObject>> PoolDic = new Dictionary<string, ObjectPool<GameObject>>();
+        static ObjectPool<GameObject> GetPool(string key)
+        {
+            var poolkey = key + "_ObjPool";
+            if (!PoolDic.ContainsKey(poolkey))
+            {
+                var prefab = Get(poolkey) as GameObject;
+                if (prefab == null)
+                {
+                    Debug.LogError(Label + "找不到预制体资源" + poolkey);
+                    PoolDic.Add(poolkey, null);
+                }
+                else
+                {
+                    var pool = PoolManager.GetPool(poolkey, prefab);
+                    PoolDic.Add(poolkey, pool);
+                }
+            }
+            return PoolDic[poolkey];
+        }
+        public static GameObject GetInstance(string key,Transform parent = null)
+        {
+            var obj = GetPool(key)?.Get();
+            if (obj = null)
+            {
+                return null;
+            }
+            obj.transform.SetParent(parent);
+            if(obj.transform is RectTransform)
+            {
+                var prefab = Get(key);
+                (obj.transform as RectTransform).anchoredPosition = (prefab.transform as RectTransform).anchoredPosition;
+            }
+            obj.name = key;
+            return obj;
+        }
+        public static GameObject GetInstance(string key, Vector3 position,Quaternion rotation,Transform parent = null)
+        {
+            var obj = GetInstance(key, parent);
+            obj.transform.position = position;
+            obj.transform.localRotation = rotation;
+            return obj;
+        }
+        public static void Push(string key,GameObject obj)
+        {
+            var pool = GetPool(key);
+            if (pool != null)
+            {
+                obj = pool.Push(obj);
+                if (obj != null && obj.transform.parent != null)
+                {
+                    obj = pool.Push(obj.transform.parent.gameObject) == null ? null : obj;
+                }
+            }
+            if (obj != null)
+            {
+                GameObject.Destroy(obj);
+                Debug.LogError("强制删除[" + key + "]:" + obj.name);
+            }
+        }
+        public static void Push(GameObject obj)
+        {
+            Push(obj.name, obj);
+        }
+        public static CT GetInstance<CT>(string key, Transform parent = null) where CT : Component
+        {
+            var obj = GetInstance(key, parent);
+            if (obj == null)
+            {
+                return null;
+            }
+            return obj.GetComponentInChildren<CT>();
+        }
+        public static CT GetInstance<CT>(string key, Vector3 pos, Quaternion rotation, Transform parent = null) where CT : Component
+        {
+            var obj = GetInstance(key, pos, rotation, parent);
+            if (obj == null)
+            {
+                return null;
+            }
+            return obj.GetComponentInChildren<CT>();
         }
     }
 }
