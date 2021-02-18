864bfad6e21579f4dc7d1ec16923d5de9f546df4
琴卓 <1043665802@qq.com>
2021-02-18 13:55:57 +0800

    完善引用

---
+++ Runtime/QAsync.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.AddressableAssets;
+#if Addressables
+namespace QTool.Async
+{
+    public abstract class AsyncList<ObjT>where ObjT:UnityEngine.Object
+    {
+        public static Dictionary<string, ObjT> objDic = new Dictionary<string, ObjT>();
+        public static string Label
+        {
+            get
+            {
+                return typeof(ObjT).ToString();
+            }
+        }
+        public static bool LabelLoadOver = false;
+        private static Action OnLabelLoadOver;
+        public static void LoadOverRun(Action action)
+        {
+            if (LabelLoadOver)
+            {
+                action?.Invoke();
+            }
+            else
+            {
+                OnLabelLoadOver += action;
+            }
+        }
+        public static bool ContainsKey(string key)
+        {
+            return objDic.ContainsKey(key);
+        }
+        public static ObjT Get(string key)
+        {
+            if (ContainsKey(key))
+            {
+                return objDic[key];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static void AsyncGet(string key,Action<ObjT> loadOver)
+        {
+            var obj = Get(key);
+            if (obj!=null)
+            {
+                loadOver?.Invoke(obj);
+            }
+            else
+            {
+             //   var load=ad
+            }
+        }
+    }
+}
+#endif
+
