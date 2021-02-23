dcf7690d5d28f3ffb7043c67104e63a863e10d66
琴卓 <1043665802@qq.com>
2021-02-23 12:36:46 +0800

    1.0.6 更改EventTrigger逻辑

--- Runtime/ValueEventTrigger.cs
+++ Runtime/ValueEventTrigger.cs
@@ -30,10 +30,10 @@ public class FloatEvent : UnityEvent<float>
 public class StringEvent : UnityEvent<string>
 {
 }
-[System.Serializable]
-public class SpriteEvent : UnityEvent<Sprite>
-{
-}
+//[System.Serializable]
+//public class SpriteEvent : UnityEvent<Sprite>
+//{
+//}
 [System.Serializable]
 public class ObjectEvent : UnityEvent<object>
 {
@@ -56,61 +56,40 @@ public class StringEventTrigger :EventTriggerBase<StringEvent>
 {
 }
 
-[System.Serializable]
-public class SpriteEventTrigger : EventTriggerBase<SpriteEvent>
-{
-}
+//[System.Serializable]
+//public class SpriteEventTrigger : EventTriggerBase<SpriteEvent>
+//{
+//}
 [System.Serializable]
 public class ObjectEventTrigger : EventTriggerBase<ObjectEvent>
 {
 }
 [System.Serializable]
-public enum InvokeNext
-{
-    不传递=0,
-    传递子物体=1,
-    传递父物体=2,
-}
 public class ValueEventTrigger : MonoBehaviour
 {
     public List<ActionEventTrigger> actionEventList=new List<ActionEventTrigger>();
     public List<StringEventTrigger> stringEventList=new List<StringEventTrigger>();
-    public List<SpriteEventTrigger> spriteEventList=new List<SpriteEventTrigger>();
+  //  public List<SpriteEventTrigger> spriteEventList=new List<SpriteEventTrigger>();
     public List<BoolEventTrigger> boolEventList=new List<BoolEventTrigger>();
     public List<FloatEventTrigger> floatEventList=new List<FloatEventTrigger>();
-    public List<ObjectEventTrigger> objectTrigger=new List<ObjectEventTrigger>();
-    ValueEventTrigger _childTigger;
-    ValueEventTrigger _parentTigger;
-    public ValueEventTrigger ChildTrigger
-    {
-        get
-        {
-            return _childTigger ?? (_childTigger = GetComponentInChildren<ValueEventTrigger>());
-        }
-    }
-    public ValueEventTrigger ParentTrigger
-    {
-        get
-        {
-            return _parentTigger ?? (_parentTigger = GetComponentInParent<ValueEventTrigger>());
-        }
-    }
-    public InvokeNext nextInoke = InvokeNext.不传递;
-    ValueEventTrigger NextTigger
-    {
-        get
-        {
-            switch (nextInoke)
-            {
-                case InvokeNext.传递子物体:
-                    return ChildTrigger;
-                case InvokeNext.传递父物体:
-                    return ParentTrigger;
-                default:
-                    return null;
-            }
-        }
-    }
+    public List<ObjectEventTrigger> objectEventList=new List<ObjectEventTrigger>();
+    //ValueEventTrigger _childTigger;
+    //ValueEventTrigger _parentTigger;
+    //public ValueEventTrigger ChildTrigger
+    //{
+    //    get
+    //    {
+    //        return _childTigger ?? (_childTigger = GetComponentInChildren<ValueEventTrigger>());
+    //    }
+    //}
+    //public ValueEventTrigger ParentTrigger
+    //{
+    //    get
+    //    {
+    //        return _parentTigger ?? (_parentTigger = GetComponentInParent<ValueEventTrigger>());
+    //    }
+    //}
+  
     
   
     public void Invoke<T>(string eventName, T value) where T: class
@@ -120,31 +99,27 @@ public class ValueEventTrigger : MonoBehaviour
         {
             stringEventList.Get(eventName)?.eventAction?.Invoke(value as string);
         }
-        else if(type == typeof(Sprite))
-        {
-            spriteEventList.Get(eventName)?.eventAction?.Invoke(value as Sprite);
-        }
+        //else if(type == typeof(Sprite))
+        //{
+        //    spriteEventList.Get(eventName)?.eventAction?.Invoke(value as Sprite);
+        //}
         else
         {
-            objectTrigger.Get(eventName)?.eventAction?.Invoke(value);
+            objectEventList.Get(eventName)?.eventAction?.Invoke(value);
         }
 
-        NextTigger?.Invoke(eventName, value);
     }
     public void Invoke(string eventName)
     {
         actionEventList.Get(eventName)?.eventAction.Invoke();
-        NextTigger?.Invoke(eventName);
     }
     public void Invoke(string eventName, bool value)
     {
         boolEventList.Get(eventName)?.eventAction?.Invoke((bool)value);
-        NextTigger?.Invoke(eventName, value);
     }
     public new void Invoke(string eventName, float value)
     {
         floatEventList.Get(eventName)?.eventAction?.Invoke(value);
-        NextTigger?.Invoke(eventName, value);
     }
 }
 public static class ValueEventTriggerExtends
