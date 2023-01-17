7cd63abd77d1fc06de5706c8e17b0761853e7cb1
琴卓 <1043665802@qq.com>
2023-01-17 15:07:53 +0800

    异常catch

--- Runtime/QTool/QTool.cs
+++ Runtime/QTool/QTool.cs
@@ -32,6 +32,10 @@ namespace QTool
 			CultureInfo.CurrentCulture = new CultureInfo("en-US");
 			CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 			QTranslate.KeyReplace["版本号"] = Application.version;
+			AppDomain.CurrentDomain.UnhandledException += (e,args) =>
+			{
+				QDebug.LogWarning(args.ExceptionObject);
+			};
 		}
 		public static string Version => Application.version; 
 		public static bool IsTestVersion => Application.version.StartsWith("0.");
