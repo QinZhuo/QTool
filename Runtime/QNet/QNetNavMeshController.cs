b21b5dc0c6d66c9548c9d891b109f87cfa130eeb
琴卓 <1043665802@qq.com>
2023-01-19 23:41:57 +0800

    1

--- Runtime/QNet/QNetNavMeshController.cs
+++ Runtime/QNet/QNetNavMeshController.cs
@@ -83,13 +83,13 @@ namespace QTool.Net
 					var targetPos = transform.position + dir.normalized*radius*2;
 					if (Physics.Raycast(targetPos, Vector3.down, out hitInfo))
 					{
-						targetPos = hitInfo.point;
-					}
-					if (NavMesh.SamplePosition(targetPos + Vector3.up * height/2, out TargetMeshHit, height, NavMesh.AllAreas) 
-						&& transform.position.y > TargetMeshHit.position.y + meshOffset)
-					{
-						TargetMeshHit.position += Vector3.up * meshOffset;
-						MeshHit = TargetMeshHit;
+						TargetMeshHit.position = hitInfo.point;
+						if (NavMesh.SamplePosition(targetPos + Vector3.up * height / 2, out TargetMeshHit, height, NavMesh.AllAreas)
+							&& transform.position.y > TargetMeshHit.position.y + meshOffset)
+						{
+							TargetMeshHit.position += Vector3.up * meshOffset;
+							MeshHit = TargetMeshHit;
+						}
 					}
 					else
 					{
