884c2d96eaa14d2fefd476fc4fb6a79a5c0ff89e
琴卓 <1043665802@qq.com>
2023-01-19 21:07:00 +0800

    1

--- Runtime/QNet/QNetNavMeshController.cs
+++ Runtime/QNet/QNetNavMeshController.cs
@@ -66,7 +66,7 @@ namespace QTool.Net
 						MeshHit = TargetMeshHit;
 					}
 				}
-				if (transform.position.y > MeshHit.position.y + 0.1f)
+				if (!Mathf.Approximately( (transform.position- MeshHit.position).sqrMagnitude,0))
 				{
 					var dir = transform.position - MeshHit.position;
 					var targetPos = transform.position + dir.normalized * radius * 2;
