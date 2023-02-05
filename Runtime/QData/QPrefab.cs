a7888fa0a79eeb144cb861eaf6589879b379bf9a
琴卓 <1043665802@qq.com>
2023-02-05 21:56:47 +0800

    去完善QPrefab存读档逻辑

--- Runtime/QData/QPrefab.cs
+++ Runtime/QData/QPrefab.cs
@@ -3,30 +3,94 @@ using System.Collections.Generic;
 using UnityEngine;
 using QTool.Reflection;
 using QTool.Inspector;
+using System.Linq;
+using System;
+
 namespace QTool
 {
-	public class QPrefab
+	public class QPrefab:IKey<string>
 	{
+		public string Key { get; set; }
+		public QList<string, QPrefab> Childs = new QList<string, QPrefab>();
 		public QList<string, QPrefabComponent> Components = new QList<string, QPrefabComponent>();
-	 	public QPrefab()
+		public QPrefab()
 		{
+			
 		}
-		public QPrefab(GameObject gameObject,GameObject prefab)
+		public QPrefab(GameObject gameObject,GameObject prefab,params Type[] ignoreComponent)
 		{
+			Key = gameObject.name;
+			var start = prefab == null ? gameObject.transform.childCount - 1 : prefab.transform.childCount - 1;
+			for (int i = start; i >= 0; i--)
+			{
+				var child = gameObject.transform.GetChild(i);
+				if (child != null)
+				{
+					var childPrefab = new QPrefab(child.gameObject, prefab?.transform?.Find(child.name)?.gameObject);
+					if (childPrefab.Components.Count > 0)
+					{
+						Childs.Add(childPrefab);
+					}
+				}
+			}
 			var components = gameObject.GetComponents<Component>();
 			foreach (var component in components)
 			{
-				var prefabComponent = new QPrefabComponent(component, prefab?.GetComponent(component.GetType()));
+				var type = component.GetType();
+				if (ignoreComponent.Contains(type)) continue;
+				QPrefabComponent prefabComponent = null;
+				if (component is IQPrefabComponent iprefab)
+				{
+					prefabComponent= iprefab.SaveQPrefab(prefab?.GetComponent(component.GetType()));
+					prefabComponent.Key = type.QTypeName();
+				}
+				else
+				{
+					prefabComponent = new QPrefabComponent(component, prefab?.GetComponent(component.GetType()));
+				}
 				if (prefabComponent.Members.Count > 0)
 				{
 					Components.Add(prefabComponent);
 				}
 			}
 		}	
-		public void Load(GameObject gameObject,GameObject prefab)
+		public void Load(GameObject gameObject)
 		{
-			
+			foreach (var childData in Childs)
+			{
+				var child = gameObject.transform.GetChild(childData.Key);
+				if (child != null)
+				{
+					childData.Load(child.gameObject);
+				}
+			}
+			foreach (var componentData in Components)
+			{
+				var type= QReflection.ParseType(componentData.Key);
+				if (type == null)
+				{
+					Debug.LogError("找不到脚本[" + componentData.Key + "]");
+				}
+				var component = gameObject.GetComponent(type);
+				if (component == null)
+				{
+					component=gameObject.AddComponent(type);
+				}
+				if (component is IQPrefabComponent iprefab)
+				{
+					iprefab.LoadQPrefab(componentData);
+				}
+				else if (component != null)
+				{
+					componentData.Load(component);
+				}
+			}
 		}
+	}
+	public interface IQPrefabComponent
+	{
+		QPrefabComponent SaveQPrefab(Component prefab);
+		void LoadQPrefab(QPrefabComponent data);
 	}
 	public class QPrefabComponent:IKey<string>
 	{
@@ -55,17 +119,33 @@ namespace QTool
 				}
 			}
 		}
+		public void Load(Component component)
+		{
+			var typeInfo = QInspectorType.Get(component.GetType());
+			foreach (var member in Members)
+			{
+				try
+				{
+					var memberInfo = typeInfo.GetMemberInfo(member.Key);
+					memberInfo.Set(component, member.Value.ParseQDataType(memberInfo.Type));
+				}
+				catch (Exception e)
+				{
+					Debug.LogError("[" + member.Key + "]" + member.Value + " :" + e);
+				}
+			}
+		}
 	}
 	public partial class Tool
 	{
-		public static QPrefab SaveQPrefab(this GameObject gameObject,GameObject prefab=null)
+		public static QPrefab SaveQPrefab(this GameObject gameObject,GameObject prefab=null,params Type[] ignoreComponent)
 		{
-			return new QPrefab(gameObject,prefab);
+			return new QPrefab(gameObject,prefab, ignoreComponent);
 		}
 		public static GameObject Instantiate(this QPrefab qprefab,GameObject prefab,Transform parent=null)
 		{
 			var gameObject = prefab.CheckInstantiate(parent);
-			qprefab.Load(gameObject, prefab);
+			qprefab.Load(gameObject);
 			return gameObject;
 		}
 	}
