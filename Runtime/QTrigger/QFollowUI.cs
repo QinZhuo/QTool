56dda0e60f8f253f38eb0a35582fde8112216b7c
琴卓 <1043665802@qq.com>
2023-02-19 02:54:44 +0800

    QFollowUI

---
+++ Runtime/QTrigger/QFollowUI.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace QTool.UI
+{
+	[ExecuteInEditMode]
+    public class QFollowUI : MonoBehaviour
+    {
+		[SerializeField]
+        private Transform target;
+		public Transform Target
+		{
+			get => target;
+			set
+			{
+				target = value;
+				if (target!=null&& useBoundsHeight)
+				{
+					bounds = target.GetBounds();
+				}
+			}
+		}
+		public RectTransform rectTransform => transform as RectTransform;
+		private Canvas _Canvas;
+		public Canvas Canvas => _Canvas??=GetComponentInParent<Canvas>();
+		[QName("使用包围盒高度")]
+		public bool useBoundsHeight=false;
+		Bounds bounds;
+		public Vector3 offset=Vector3.zero;
+		private void Awake()
+		{
+			if(Canvas!=null&&Canvas.renderMode!= RenderMode.WorldSpace)
+			{
+				rectTransform.anchorMin = Vector2.zero;
+				rectTransform.anchorMax = Vector2.zero;
+			}
+		}
+		private void LateUpdate()
+        {
+            if (target != null&&target.gameObject.activeInHierarchy)
+            {
+				var runtimeOffset = offset;
+				if (useBoundsHeight)
+				{
+					runtimeOffset += bounds.size.y*Vector3.up;
+				}
+				var position = target.position + runtimeOffset;
+				if(Canvas!=null&&Canvas.renderMode == RenderMode.WorldSpace)
+				{
+					rectTransform.position = position;
+				}
+				else
+				{
+					rectTransform.anchoredPosition= Camera.main.WorldToScreenPoint(position);
+				}
+            }
+        }
+    }
+
+}
---
+++ Runtime/QTrigger/QFollowUIList.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace QTool.UI
+{
+	[RequireComponent(typeof(QObjectList))]
+	public class QFollowUIList :MonoBehaviour
+	{
+		private QObjectList _ObjectList;
+		public QObjectList ObjectList => _ObjectList ??= GetComponent<QObjectList>();
+		public void Push(GameObject obj)
+		{
+			ObjectList.Push(obj);
+		}
+		public QFollowUI this[Transform target]
+		{
+			get
+			{
+				if (target == null)
+				{
+					Debug.LogError("follow目标为空");
+					return null;
+				}
+				var obj = ObjectList[target.GetHashCode().ToString()];
+				var followUI = obj.GetComponent<QFollowUI>();
+				if (followUI != null)
+				{
+					followUI.Target = target;
+					return followUI;
+				}
+				else
+				{
+					throw new System.Exception(target + " 目标不存在脚本 " + typeof(QFollowUI));
+				}
+			}
+		}
+	}
+
+}
