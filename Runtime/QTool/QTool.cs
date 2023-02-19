1250fa48dfb4d0c811cc1917d729b340bc95a514
琴卓 <1043665802@qq.com>
2023-02-20 00:20:06 +0800

    1

--- Runtime/QTool/QTool.cs
+++ Runtime/QTool/QTool.cs
@@ -111,7 +111,7 @@ namespace QTool
 			{
 				playerLoop = playerLoop.AddPlayerLoop(type, action);
 			}
-			QDebug.Log(playerLoop.subSystemList.ToOneString("\n", (loop) => "----------" + playerLoop + "----------\n" + playerLoop.subSystemList.ToOneString()));
+			QDebug.Log(playerLoop.subSystemList.ToOneString("\n", (loop) => "----------" + loop + "----------\n" + loop.subSystemList.ToOneString()));
 			PlayerLoop.SetPlayerLoop(playerLoop);
 		}
 		public static void RemovePlayerLoop(Type type, string subSystem = "")
