5ca64652770a6602054d012d4234e730105d0231
琴卓 <1043665802@qq.com>
2023-02-26 21:12:44 +0800

    1

--- Runtime/QTool/QTool.cs
+++ Runtime/QTool/QTool.cs
@@ -252,7 +252,7 @@ namespace QTool
 			{
 				value /= size;
 			}
-			return Mathf.RoundToInt(value-0.5f);
+			return Mathf.RoundToInt(value-0.25f);
 		}
 		public static float GridFixed(this float value, float size)
 		{
