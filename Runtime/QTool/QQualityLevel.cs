23d06668d8c6718ff656c71e136e0ff2dd7d52d4
琴卓 <1043665802@qq.com>
2023-03-16 17:00:53 +0800

    1

--- Runtime/QTool/QQualityLevel.cs
+++ Runtime/QTool/QQualityLevel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-namespace QTool.TileMap {
+namespace QTool {
 	[ExecuteInEditMode]
 	public class QQualityLevel : MonoBehaviour
 	{
