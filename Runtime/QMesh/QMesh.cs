cd1f03d24a6e99560dcfb0bfd75a38705da8ccbc
琴卓 <1043665802@qq.com>
2023-04-09 20:08:19 +0800

    1

--- Runtime/QMesh/QMesh.cs
+++ Runtime/QMesh/QMesh.cs
@@ -172,6 +172,7 @@ namespace QTool
 			root.sharedMesh.RecalculateBounds();
 			root.bones = bones.ToArray();
 			root.materials = matList.ToArray();
+			root.localBounds =root.sharedMesh.bounds;
 		}
 	}
 }
