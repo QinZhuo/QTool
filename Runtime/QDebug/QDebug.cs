bc5641630849acfac5bb1e87fd54a8201447056e
琴卓 <1043665802@qq.com>
2023-04-14 19:35:13 +0800

    帧率

--- Runtime/QDebug/QDebug.cs
+++ Runtime/QDebug/QDebug.cs
@@ -361,7 +361,7 @@ namespace QTool
 		[System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
 		public static void Log(object obj,long startTimestamp)
 		{
-			Debug.Log("[" + nameof(QDebug) + "]  " + obj+" 时间 "+GetIntervalSeconds(startTimestamp).ToString("f3")+" s");
+			Debug.Log("[" + nameof(QDebug) + "]  " + obj+" 时间 "+GetIntervalSeconds(startTimestamp).ToString("f3")+" s"+" 帧率 "+FPS);
 		}
 		[System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
 		public static void LogWarning(object obj)
