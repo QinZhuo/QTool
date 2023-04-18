2bbddb19d4075b9c4edf414b03f355b343bc4414
琴卓 <1043665802@qq.com>
2023-04-19 06:40:48 +0800

    Values

--- Runtime/QFlowGraph/QFlowGraphNode.cs
+++ Runtime/QFlowGraph/QFlowGraphNode.cs
@@ -218,7 +218,7 @@ namespace QTool.FlowGraph
 				trigger.Target = target;
 				yield return trigger.Run((t) =>
 				{
-					This.Graph.Values[nameof(triggerObject)] = t;
+					This[nameof(triggerObject)] = t;
 					This.RunPort(nameof(triggerObject));
 				});
 			}
