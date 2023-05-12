2ebf13a810a5a1248179026c340f128e08ddd594
琴卓 <1043665802@qq.com>
2023-05-13 06:20:48 +0800

    1

--- Runtime/QFlowGraph/QFlowGraph.cs
+++ Runtime/QFlowGraph/QFlowGraph.cs
@@ -984,7 +984,7 @@ namespace QTool.FlowGraph
 				if (Ports.ContainsKey(valueKey))
 				{
 					Ports[valueKey].Value = value;
-					Debug.LogError("value[" + valueKey + "]=" + value);
+					Debug.LogError("value[" + valueKey + "]=" + value+" "+ Ports[valueKey].Value);
 				}
 				else
 				{
