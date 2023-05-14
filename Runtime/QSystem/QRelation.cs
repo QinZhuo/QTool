b2d5032fcab4fdc7f095ce73b40a2b901e8ac10a
琴卓 <1043665802@qq.com>
2023-05-15 00:34:20 +0800

    Team逻辑

--- Runtime/QSystem/QRelation.cs
+++ Runtime/QSystem/QRelation.cs
@@ -7,22 +7,11 @@ namespace QTool
 	/// <summary>
 	/// 关系
 	/// </summary>
-	public class QRelation:IKey<string>
+	public class QRelation : IKey<string>
 	{
-		public static QList<string, QRelation> Relations = new QList<string, QRelation>(()=>new QRelation());
-		public static float Get(string a,string b)
-		{
-			return Relations[a][b];
-		}
-		public string Key { get;  set; }
-		private QRelation()
-		{
-
-		}
-		public QRelation(string key)
-		{
-			Key = key;
-		}
+		public static QList<string, QRelation> Relations = new QList<string, QRelation>(() => new QRelation());
+		public string Key { get; set; }
+		private QRelation() { }
 		public float this[string key]
 		{
 			get => this[Relations[key]];
@@ -43,5 +32,28 @@ namespace QTool
 			}
 		}
 		private QDictionary<QRelation, float> Values = new QDictionary<QRelation, float>();
+		public static float Get(string a, string b)
+		{
+			return Relations[a][b];
+		}
+	}
+	/// <summary>
+	/// 队伍
+	/// </summary>
+	public static class QTeam<T>
+	{
+		public static QDictionary<string, List<T>> Teams = new QDictionary<string, List<T>>((key)=>new List<T>());
+		public static void GetList(string teamKey, List<T> list, float min = 0, float max = 10)
+		{
+			list.Clear();
+			foreach (var team in Teams)
+			{
+				var relation = QRelation.Get(team.Key, teamKey);
+				if (relation <= max && relation >= min)
+				{
+					list.AddRange(team.Value);
+				}
+			}
+		}
 	}
 }
