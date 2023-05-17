ac2e2f93756712f48a58aba79f5526b37e9f6c9a
琴卓 <1043665802@qq.com>
2023-05-18 01:49:13 +0800

    目标

--- Runtime/QTrigger/QTrigger.cs
+++ Runtime/QTrigger/QTrigger.cs
@@ -7,6 +7,7 @@ namespace QTool
 	public abstract class QTrigger : MonoBehaviour
 	{
 		public Transform Start { get; set; }
+		public Transform Target { get; set; }
 		public abstract IEnumerator Run(Action<Transform> action);
 	}
 }
