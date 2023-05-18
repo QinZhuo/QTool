8425de93cee2f5e44a9fb14955572ccbbf58b592
琴卓 <1043665802@qq.com>
2023-05-19 05:21:31 +0800

    1

--- Runtime/QFlowGraph/QFlowGraphNode.cs
+++ Runtime/QFlowGraph/QFlowGraphNode.cs
@@ -233,7 +233,7 @@ namespace QTool.FlowGraph
 				case nameof(Trigger):
 					return node.Ports["triggerObject"].ToInfoString();
 				case nameof(GetValue):
-					return "{" + node.Ports["key"] + "}";
+					return "{" + node["key"] + "}";
 				default:
 					return node.Name;
 			}
