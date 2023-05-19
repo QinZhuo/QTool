9c47231d221f70ad163330310bc257053576d5a2
琴卓 <1043665802@qq.com>
2023-05-19 19:06:04 +0800

    1

--- Runtime/QSystem/QBuffSystem.cs
+++ Runtime/QSystem/QBuffSystem.cs
@@ -15,8 +15,6 @@ namespace QTool
 	}
 	public abstract class QEffectData<T> : QDataList<T> where T : QEffectData<T>, IKey<string>, new()
 	{
-		[QName("显示名")]
-		public virtual string Name { get; set; }
 		[QIgnore]
 		private string _EffctInfo = "";
 		[QName("效果说明")]
