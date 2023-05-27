a8e77c6cf4237e5dc197fa8cd3aac2659dad8d70
琴卓 <1043665802@qq.com>
2023-05-27 23:31:20 +0800

    1

--- Runtime/QMesh/QMesh.cs
+++ Runtime/QMesh/QMesh.cs
@@ -184,7 +184,7 @@ namespace QTool
 				foreach (var bone in skinedMesh.bones)
 				{
 					bones.Add(childs.Get(bone.name, (trans) => trans.name));
-					var bind = skinedMesh.worldToLocalMatrix;
+					var bind = bone.worldToLocalMatrix* skinedMesh.worldToLocalMatrix*skinedMesh.transform.parent.worldToLocalMatrix;
 					//if (skinedMesh.transform.parent.parent != null)
 					//{
 					//	bind *= skinedMesh.transform.parent.parent.localToWorldMatrix;
