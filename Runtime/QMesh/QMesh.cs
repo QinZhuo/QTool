6d172efcd3d50e746780b7957de2b9d744897ed6
琴卓 <1043665802@qq.com>
2023-05-27 22:37:16 +0800

    1

--- Runtime/QMesh/QMesh.cs
+++ Runtime/QMesh/QMesh.cs
@@ -155,7 +155,7 @@ namespace QTool
 				{
 					var combine = new CombineInstance();
 					combine.mesh = skinedMesh.sharedMesh;
-			//		combine.transform = skinedMesh.localToWorldMatrix;
+					combine.transform = skinedMesh.localToWorldMatrix;
 				//	combine.subMeshIndex = sub;
 					combineInfos.Add(combine);
 				}
