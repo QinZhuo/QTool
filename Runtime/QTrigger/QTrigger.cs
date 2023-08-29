5af685dd958492077253fc74b0e06dcb922470f3
琴卓 <1043665802@qq.com>
2023-08-29 17:38:08 +0800

    1

--- Runtime/QTrigger/QTrigger.cs
+++ Runtime/QTrigger/QTrigger.cs
@@ -7,9 +7,9 @@ namespace QTool
 {
 	public abstract class QTrigger : MonoBehaviour
 	{
-		public Transform Start { get; set; }
-		public Transform Target { get; set; }
-		public QFlowNode Node { get;internal set; }
+		public virtual Transform Start { get; set; }
+		public virtual Transform Target { get; set; }
+		public QFlowNode Node { get; set; }
 		public QFlowGraph Graph => Node?.Graph;
 		public abstract IEnumerator Run(Action<Transform> action);
 	}
