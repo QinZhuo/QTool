68941890586737d7cdee60005b2a7facb72d432a
qinzhuo <1043665802@qq.com>
2023-09-21 22:36:09 +0800

    1

--- Runtime/QMesh/QSpriteShapeRange.cs
+++ Runtime/QMesh/QSpriteShapeRange.cs
@@ -41,48 +41,6 @@ namespace QTool
 						Controller.spline.InsertPointAt(index++, start);
 						Controller.spline.InsertPointAt(index++, end);
 						Controller.spline.InsertPointAt(index++, right);
-					}
-					break;
-				case Shape.扇形:
-					{
-						Controller.spline.InsertPointAt(index++, Vector2.zero);
-						Controller.spline.SetTangentMode(index - 1, ShapeTangentMode.Linear);
-						var endAngle = Mathf.Atan2(width / 2, distance) / Mathf.PI * 360 * 2;
-						var dir = (Vector2.up * distance).Rotate(-endAngle / 2);
-						Controller.spline.InsertPointAt(index++, dir);
-						Controller.spline.SetTangentMode(index - 1, ShapeTangentMode.Linear);
-						for (float angle = 5; angle <= endAngle; angle += 5)
-						{
-							Controller.spline.InsertPointAt(index++, dir.Rotate(angle));
-							Controller.spline.SetTangentMode(index - 1, ShapeTangentMode.Continuous);
-						}
-						Controller.spline.InsertPointAt(index++, dir.Rotate(endAngle));
-						Controller.spline.SetTangentMode(index - 1, ShapeTangentMode.Linear);
-					}
-					break;
-				case Shape.圆形:
-					{
-						var dir = Vector2.up * Mathf.Max(distance, width / 2);
-						for (float angle = 0; angle < 360; angle += 5)
-						{
-							Controller.spline.InsertPointAt(index++, dir.Rotate(angle));
-							Controller.spline.SetTangentMode(index - 1, ShapeTangentMode.Continuous);
-						}
-					}
-					break;
-				default:
-					break;
-			}
-			switch (shape)
-			{
-				case Shape.矩形:
-					{
-						var start = left + Vector2.up * distance;
-						var end = right + Vector2.up * distance;
-						Controller.spline.InsertPointAt(index++, left);
-						Controller.spline.InsertPointAt(index++, start);
-						Controller.spline.InsertPointAt(index++, end);
-						Controller.spline.InsertPointAt(index++, right);
 						
 					}
 					break;
@@ -92,11 +50,16 @@ namespace QTool
 						var endAngle = Mathf.Atan2(width / 2, distance) / Mathf.PI * 360 * 2;
 						var dir = (Vector2.up * distance).Rotate(-endAngle / 2);
 						Controller.spline.InsertPointAt(index++, dir);
-						for (float angle = 5; angle <= endAngle; angle += 5)
+						float angle = 5;
+						for (; angle <= endAngle; angle += 5)
 						{
 							Controller.spline.InsertPointAt(index++, dir.Rotate(angle));
 						}
-						Controller.spline.InsertPointAt(index++, dir.Rotate(endAngle));
+						var end = dir.Rotate(endAngle);
+						if (Vector2.Distance(dir.Rotate(angle), end) > 1f)
+						{
+							Controller.spline.InsertPointAt(index++, end);
+						}
 						for (int i = 0; i < index; i++)
 						{
 							switch (i)
