24e17c6082c456fd4c2c6d39930fc06f7f533e70
qinzhuo <1043665802@qq.com>
2023-10-12 05:57:59 +0800

    1

--- Runtime/QData/QKeyValueTrigger.cs
+++ Runtime/QData/QKeyValueTrigger.cs
@@ -1,7 +1,7 @@
 using QTool;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Plastic.Newtonsoft.Json.Serialization;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
