f89751202d3cc71f1d510a0c8821a45e68d8377b
琴卓 <1043665802@qq.com>
2023-12-25 12:48:00 +0800

    修改 Runtime/QNet/QNetManager.cs

--- Runtime/QNet/QNetManager.cs
+++ Runtime/QNet/QNetManager.cs
@@ -18,7 +18,7 @@ namespace QTool.Net
 		public QNetTransport transport; 
 		[QName("网络帧率"),SerializeField,Tooltip("每秒进行多少次网络帧同时更改物理帧率 两者保持同步")]
 		[Range(15,60)]
-		private int netFps = 30;
+		private int netFps = 30; 
 		public System.Random Random { get; private set; } = null;
 		protected override void Awake()
 		{
