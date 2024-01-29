e05e3b7836f86317da2916a32d3fef2df9192018
琴卓 <1043665802@qq.com>
2024-01-30 00:52:46 +0800

    1

--- Runtime/QLocalization/QLocalization.cs
+++ Runtime/QLocalization/QLocalization.cs
@@ -221,7 +221,10 @@ namespace QTool
 			}
 			return "{" + string.Format(key, Params) + "}";
 		}
-		
+		public static string ToLozalizationColorKey(this object key)
+		{
+			return ("{" + key + "}").ToColorString(key.ToColor());
+		}
 		const string NetworkTranslateURL = "https://translate.googleapis.com/translate_a/single?client=gtx&sl={2}&tl={1}&dt=t&q={0}";
 
 		static List<List<List<string>>> translateData = new List<List<List<string>>>();
